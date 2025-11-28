8d27e56e9893082340db83054832b77efe5a7ce7
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2025-11-28 18:47:36 -0300

    lista-compras update

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.scss',
 })
 export class ListaCompras {
 
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    console.table(this.lista);
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
 }
